8c1b36607ee4d5de4e0aff6c93a08a13e6a35866
Daniel Yan <daniel.yan@nextreality.com>
2022-07-13 14:59:13 +0100

    Add App Component Version update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularApp'`, () => {
+  it(`should have a 'Status'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularApp');
+    expect(app.isOnline).toEqual(true);
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('AngularApp app is running!');
+    expect(compiled.querySelector('.container-fluid h1')?.textContent).toContain('Angular Progressive Web Application (PWA)');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Platform } from '@angular/cdk/platform';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,21 +11,94 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   isOnline: boolean;
+  modalVersion: boolean;
+  modalPwaEvent: any;
+  modalPwaPlatform: string | undefined;
 
-  constructor() {
+  constructor(private platform: Platform,
+    private swUpdate: SwUpdate) {
     this.isOnline = false;
+    this.modalVersion = false;
   }
 
   public ngOnInit(): void {
     this.updateOnlineStatus();
 
-    window.addEventListener('online',  this.updateOnlineStatus.bind(this));
+    window.addEventListener('online', this.updateOnlineStatus.bind(this));
     window.addEventListener('offline', this.updateOnlineStatus.bind(this));
+
+    if (this.swUpdate.isEnabled) { //Must run on production mode to be activated (app.module)
+      this.swUpdate.versionUpdates.pipe(
+        filter((evt: any): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
+        map((evt: any) => {
+          console.info(`currentVersion=[${evt.currentVersion} | latestVersion=[${evt.latestVersion}]`);
+          this.modalVersion = true;
+        }),
+      );
+
+      const updatesAvailable = this.swUpdate.versionUpdates.pipe(
+        filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
+        map(evt => ({
+          type: 'UPDATE_AVAILABLE',
+          current: evt.currentVersion,
+          available: evt.latestVersion,
+        })));
+
+      updatesAvailable.subscribe(() => {
+
+        console.info('new version');
+        if (confirm("New version available. Load New Version?")) {
+
+          window.location.reload();
+        }
+      });
+    }
   }
 
+  /**
+   * VERSION UPDATE
+   */
   private updateOnlineStatus(): void {
     this.isOnline = window.navigator.onLine;
     console.info(`isOnline=[${this.isOnline}]`);
   }
+
+  public updateVersion(): void {
+    this.modalVersion = false;
+    window.location.reload();
+  }
+
+  public closeVersion(): void {
+    this.modalVersion = false;
+  }
+
+  // /**
+  //  * ADD TO HOME - NO NEED em recent angular versions
+  //  */
+  //  private loadModalPwa(): void {
+  //   if (this.platform.ANDROID) {
+  //     window.addEventListener('beforeinstallprompt', (event: any) => {
+  //       event.preventDefault();
+  //       this.modalPwaEvent = event;
+  //       this.modalPwaPlatform = 'ANDROID';
+  //     });
+  //   }
+
+  //   if (this.platform.IOS && this.platform.SAFARI) {
+  //     const isInStandaloneMode = ('standalone' in window.navigator) && ((<any>window.navigator)['standalone']);
+  //     if (!isInStandaloneMode) {
+  //       this.modalPwaPlatform = 'IOS';
+  //     }
+  //   }
+  // }
+
+  // public addToHomeScreen(): void {
+  //   this.modalPwaEvent.prompt();
+  //   this.modalPwaPlatform = undefined;
+  // }
+
+  // public closePwa(): void {
+  //   this.modalPwaPlatform = undefined;
+  // }
 
 }
